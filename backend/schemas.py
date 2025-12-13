8e300e5e0ce76af9d051bfe6dcc938c7fb0bc0b8
Osmolovsky K. <kirillosm09@gmail.com>
2025-12-13 15:20:45 +1000

    feat: move to v2 api, deprecate v1

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,11 +1,36 @@
 from pydantic import BaseModel, EmailStr, Field
+from datetime import date
 
+
+### SCHEMAS FOR USERS
 
 class UserRegister(BaseModel):
     """pydantic class for registration"""
     email: EmailStr
     password: str = Field(min_length=8, max_length=128)
 
+
 class UserOut(BaseModel):
     id: int
     email: EmailStr
+
+
+### SCHEMAS FOR NOTES
+
+class NoteCreate(BaseModel):
+    note_text: str
+    note_date: date
+
+
+class NoteUpdate(BaseModel):
+    note_text: str
+
+
+class NoteOut(BaseModel):
+    note_id: int
+    note_text: str
+    note_date: date
+
+
+class StatusOut(BaseModel):
+    status: bool
