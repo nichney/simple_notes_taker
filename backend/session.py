34e4811a268b905ad94d228b6805bac119358a1f
Osmolovsky K. <kirillosm09@gmail.com>
2025-12-13 14:15:58 +1000

    fix: secure database env throught environment variables

--- backend/session.py
+++ backend/session.py
@@ -1,6 +1,7 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://postgres:password@localhost:5432/notesdb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
