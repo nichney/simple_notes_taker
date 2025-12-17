c0d07e8c02a78e8b42f1425538b4ca25e5be22f9
Osmolovsky K. <kirillosm09@gmail.com>
2025-12-17 13:11:34 +1000

    test: add integration tests for api

--- backend/tests/integration/test_api.py
+++ backend/tests/integration/test_api.py
@@ -65,6 +65,47 @@ def registered_user(client):
         'id': response.json()['id']
     }
 
+@pytest.fixture
+def logged_in_user_data(client: TestClient, registered_user: dict):
+    response = client.post(
+        '/api/v2/auth/login',
+        json={'email': registered_user['email'], 'password': registered_user['password']}
+    )
+    
+    if response.status_code != 200:
+        pytest.fail(f"Login failed in fixture: {response.json()}")
+        
+    tokens = response.json()
+    
+    return {
+        'id': registered_user['id'],
+        'email': registered_user['email'],
+        'access_token': tokens['access_token'],
+        'refresh_token': tokens['refresh_token'],
+        'auth_header': {"Authorization": f"Bearer {tokens['access_token']}"}
+    }
+
+@pytest.fixture
+def note_fixture(client: TestClient, logged_in_user_data: dict):
+    response = client.post(
+        '/api/v2/create',
+        headers=logged_in_user_data['auth_header'],
+        json={
+            'note_text': 'First Note',
+            'note_date': '2025-12-17'
+        }
+    )
+    tokens = response.json()
+    return {
+        'note_id': tokens['note_id'],
+        'note_text': tokens['note_text'],
+        'note_date': tokens['note_date'],
+        'user_id': logged_in_user_data['id'],
+        'access_token': logged_in_user_data['access_token'],
+        'refresh_token': logged_in_user_data['refresh_token'],
+        'email': logged_in_user_data['email'],
+        'auth_header': logged_in_user_data['auth_header']
+    }
 
 def test_register(client):
     response = client.post(
@@ -118,5 +159,97 @@ def test_login_invalid_email(client):
     response = client.post(
         '/api/v2/auth/login',
         json={'email': 'nouser@sample.com', 'password': 'sample_password'}
+    )
+    assert response.status_code == 401
+
+
+def test_create_note_success(client, logged_in_user_data):
+    response = client.post(
+        '/api/v2/create',
+        headers=logged_in_user_data['auth_header'],
+        json={
+            'note_text': 'First Note',
+            'note_date': '2025-12-17'
+        }
+    )
+    
+    assert response.status_code == 201
+    assert response.json()['note_text'] == 'First Note'
+
+def test_create_note_unauthorized(client):
+    response = client.post(
+        '/api/v2/create',
+        json={
+            'note_text': 'First Note',
+            'note_date': '2025-12-17'
+        }
+    )
+    
+    assert response.status_code == 401
+
+
+def test_get_note(client, note_fixture):
+    response = client.get(
+        f'/api/v2/{note_fixture["note_id"]}',
+        headers=note_fixture['auth_header']
+    )
+    assert response.status_code == 200
+    assert response.json()['note_text'] == note_fixture['note_text']
+
+def test_get_note_invalid_id(client, note_fixture):
+    response = client.get(
+        f'/api/v2/{999}',
+        headers=note_fixture['auth_header']
+    )
+    assert response.status_code == 404
+
+def test_get_note_unauthorized(client, note_fixture):
+    response = client.get(
+        f'/api/v2/{note_fixture["note_id"]}'
+    )
+    assert response.status_code == 401
+
+
+def test_update_note(client, note_fixture):
+    response = client.put(
+        f'/api/v2/{note_fixture["note_id"]}',
+        headers=note_fixture['auth_header'],
+        json={'note_text': 'new note text'}
+    )
+    assert response.status_code == 200
+    assert response.json()['status']
+
+def test_update_note_invalid_id(client, note_fixture):
+    response = client.put(
+        f'/api/v2/{999}',
+        headers=note_fixture['auth_header'],
+        json={'note_text': 'new note text'}
+    )
+    assert response.status_code == 404
+
+def test_update_note_unauthorized(client, note_fixture):
+    response = client.put(
+        f'/api/v2/{note_fixture["note_id"]}',
+        json={'note_text': 'new note text'}
+    )
+    assert response.status_code == 401
+
+
+def test_delete_note(client, note_fixture):
+    response = client.delete(
+        f'/api/v2/{note_fixture["note_id"]}',
+        headers=note_fixture['auth_header']
+    )
+
+def test_delete_note_invalid_id(client, note_fixture):
+    response = client.delete(
+        f'/api/v2/{999}',
+        headers=note_fixture['auth_header']
+    )
+    assert response.status_code == 404
+
+def test_delete_note_unauthorized(client, note_fixture):
+    response = client.delete(
+        f'/api/v2/{note_fixture["note_id"]}'
     )
     assert response.status_code == 401
