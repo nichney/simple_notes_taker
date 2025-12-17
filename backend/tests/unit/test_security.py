39440e7ad4805a403e3caf7105bf453498390b07
Osmolovsky K. <kirillosm09@gmail.com>
2025-12-17 13:55:55 +1000

    test: add unit tests for exiped jwt

--- backend/tests/unit/test_security.py
+++ backend/tests/unit/test_security.py
@@ -1,4 +1,7 @@
 import pytest
+from datetime import datetime, timedelta, timezone
+import jwt
+import os
 
 from security import (
     hash_password,
@@ -8,6 +11,8 @@ from security import (
     decode_token,
 )
 
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
 
 @pytest.fixture(scope="module")
 def common_data():
@@ -54,3 +59,26 @@ class TestSecurity:
     def test_decode_token_id(self, common_data):
         decoded_id = decode_token(common_data['access_token']).get('sub')
         assert decoded_id == str(common_data['user_id'])
+
+    def create_test_token(self, payload: dict, expires_delta: timedelta):
+        expire = datetime.now(timezone.utc) + expires_delta
+        payload.update({"exp": expire.timestamp(), "type": "access"}) # "iat": datetime.now(timezone.utc).timestamp(),
+        
+        return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+    def test_decode_token_expired(self):
+        test_payload = {"sub": "123"}
+        expired_token = self.create_test_token(test_payload, timedelta(minutes=-1))
+        with pytest.raises(jwt.exceptions.ExpiredSignatureError):
+            decode_token(expired_token)
+
+    def test_decode_token_invalid_signature(self):
+        test_payload = {"sub": "123"}
+        valid_token = self.create_test_token(test_payload, timedelta(minutes=1))
+        with pytest.raises(jwt.exceptions.InvalidSignatureError):
+            jwt.decode(valid_token, "WRONG_SECRET", algorithms=[ALGORITHM])
+
+    def test_decode_token_invalid_format(self):
+        invalid_token = "invalid.format"
+        with pytest.raises(jwt.exceptions.InvalidTokenError):
+            decode_token(invalid_token)
